e34fb92d9e23ec81b5328eb4caba9cdba8c54f71
denis-mwangi <denwebservices@gmail.com>
2025-03-13 23:00:07 +0300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,29 @@
+# Question 1
+my_list = []
+
+# Question 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# Question 3
+my_list.insert(2, 15)
+print(my_list)
+
+# Question 4
+my_list2 = [50, 60, 70]
+my_list.extend(my_list2)
+print(my_list)
+
+# Question 5
+del my_list[-1]
+print(my_list)
+
+# Question 6
+my_list.sort()
+print(my_list)
+
+# Question 7
+print(my_list.index(30))
